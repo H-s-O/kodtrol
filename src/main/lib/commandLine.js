e4fec9a0bad00d92c65ff050e94717252042b387
H-s-O <hugostonge@gmail.com>
2024-08-27 18:16:22 -0400

    "run-board" cli option

--- src/main/lib/commandLine.js
+++ src/main/lib/commandLine.js
@@ -4,20 +4,27 @@ import isDev from '../../common/js/lib/isDev';
 
 const argv = yargs(process.argv.slice(isDev ? 2 : 1)).argv;
 
-export const screenshotsFile = () => {
+export const cliScreenshotsFile = () => {
     if ('screenshots' in argv && !!argv.screenshots) {
         return argv.screenshots;
     }
     return null;
 };
 
-export const projectFile = () => {
+export const cliProjectFile = () => {
     if ('project' in argv && !!argv.project) {
         return argv.project;
     }
     return null;
 };
 
-export const newProject = () => {
+export const cliNewProject = () => {
     return 'newProject' in argv && argv.newProject === true;
+}
+
+export const cliRunBoard = () => {
+    if ('runBoard' in argv && !!argv.runBoard) {
+        return argv.runBoard;
+    }
+    return null;
 }
