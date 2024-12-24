fc2f8261b05a75c82faf61331e083f3c93a4de48
H-s-O <hugostonge@gmail.com>
2024-12-23 21:11:56 -0500

    updated default script template

--- src/common/js/constants/app.js
+++ src/common/js/constants/app.js
@@ -3,19 +3,25 @@ export const PROJECT_FILE_EXTENSION = 'kodtrol';
 
 export const SCRIPT_TEMPLATE =
 `// function start(devices) {
+//  
 // }
 
-// function leadInFrame(devices, { /* block info */ }, { /* triggers */ }, { /* curves */ }) {
+// function leadInFrame(devices, { blockPercent, inTime, outTime, currentTime }, triggers, curves) {
+//  
 // }
 
-// function frame(devices, { /* block info */ }, { /* triggers */ }, { /* curves */ }) {
-// }
+function frame(devices, { blockPercent, inTime, outTime, currentTime }, triggers, curves) {
+  
+}
 
-// function leadOutFrame(devices, { /* block info */ }, { /* triggers */ }, { /* curves */ }) {
+// function leadOutFrame(devices, { blockPercent, inTime, outTime, currentTime }, triggers, curves) {
+//  
 // }
 
 // function beat(devices, { localBeat, globalBeat }) {
+//  
 // }
 
 // function input(devices, inputType, inputData) {
+//  
 // }`;
